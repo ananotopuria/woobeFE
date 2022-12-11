15e206cfc9ac9d908ba6992acc7d1ac5b0c04b6c
Anano Topuria <anano.topuriaa@gmail.com>
2022-12-11 19:02:34 +0400

    header

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-document.getElementById("create").onclick = function (e) {
-	// e.preventDefault();
-	console.log("🚀 ~ file: script.js:4 ~ createBtn.onclick")
-	//
-};
+// document.getElementById("create").onclick = function (e) {
+// 	// e.preventDefault();
+// 	console.log("🚀 ~ file: script.js:4 ~ createBtn.onclick");
+// 	//
+// };
+
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(`.section-hero`);
+const obs = new IntersectionObserver(
+	function (entries) {
+		const ent = entries[0];
+		console.log(ent);
+		if (ent.isIntersecting === false) {
+			document.body.classList.add(`sticky`);
+		}
+		if (ent.isIntersecting) {
+			document.body.classList.remove(`sticky`);
+		}
+	},
+
+	{
+		// in the viewport
+		root: null,
+		threshold: 0,
+		rootMargin: `-80px`,
+	}
+);
+
+obs.observe(sectionHeroEl);
